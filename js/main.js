27bbed6d9f879e9c3b0f017c64cbb8dd55258904
Prudhvi Vuda <53619858+Prudhvivuda@users.noreply.github.com>
2020-08-07 22:50:44 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -67,7 +67,7 @@ $(document).ready(function() {
 
   $(function() {
     typed.typed({
-      strings: ["prudhvi vuda.",  "a programmer.", "a pilosopher." ,"an artificial intelligence enthusiast."],
+      strings: ["prudhvi vuda.",  "a programmer.", "a philosopher." ,"an artificial intelligence enthusiast."],
       typeSpeed: 100,
       loop: true,
     });
